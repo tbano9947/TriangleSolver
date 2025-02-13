215aa3c7dc7561f7f83d387e337f66ba0dc80bea
Tasnim Bano Tasnim Bano <Tbano9947@conestogac.on.ca>
2025-02-13 02:11:00 -0500

    Add project files

---
+++ TriangleSolverTests/TestClass.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using TriangleSolver;
+
+namespace TriangleSolverTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        // 1 Test for a valid Equilateral triangle
+        [Test]
+        public void AnalyzeTriangle_Equilateral_ReturnsEquilateral()
+        {
+            // Arrange
+            int side1 = 5, side2 = 5, side3 = 5;
+            string expected = "Equilateral triangle";
+
+            // Act
+            string actual = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+        // 3 Tests for a valid Isosceles triangle
+        [Test]
+        public void AnalyzeTriangle_Isosceles_TwoEqualSides1_ReturnsIsosceles()
+        {
+            // Arrange
+            int side1 = 5, side2 = 5, side3 = 3;
+            string expected = "Isosceles triangle";
+            // Act
+            string actual = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            
+            ClassicAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Isosceles_TwoEqualSides2_ReturnsIsosceles()
+        {
+            // Arrange
+            int side1 = 6, side2 = 4, side3 = 6;
+            string expected = "Isosceles triangle";
+            // Act
+            string actual = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            
+            ClassicAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Isosceles_TwoEqualSides3_ReturnsIsosceles()
+        {
+            // Arrange
+            int side1 = 7, side2 = 9, side3 = 9;
+            string expected = "Isosceles triangle";
+            // Act
+            string actual = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+         
+            ClassicAssert.AreEqual(expected, actual);
+        }
+
+    }
+}
